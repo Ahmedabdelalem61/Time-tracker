27d62b2f2cebb0900caee4660b168eb77941470b
Ahmed Abdelalem <Ahmed.Abdelalem60@gmail.com>
2022-01-11 01:27:39 +0200

    refactoring alert to be compatible with all messages

--- lib/app/home_page.dart
+++ lib/app/home_page.dart
@@ -1,19 +1,38 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:time_tracker/common_widgets/show_alert_dialog.dart';
 import 'package:time_tracker/services/auth.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({Key? key,required this.auth}) : super(key: key);
   late final AuthBase auth;
 
-  Future<void> _signOutAnonymously()async {
+  Future<void> _signOut()async {
     try{
       await auth.signOut();
     }catch(e){
         print(e);
     }
 
-}
+  }
+// the alert dialog return always with null
+  Future<void> _confirmSignOut(BuildContext context)async{
+    // final _didRequestSignOut = await showAlertDialog(
+    //   context:context,
+    //   content: 'do you want to sign out ?',
+    //   defaultActionText: 'Ok',
+    //   title: 'sign out',
+    //   cancelText: 'cancel',
+    // );
+    //
+    //
+    // if(_didRequestSignOut == true){
+    //   _signOut();
+    // }
+
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -21,11 +40,9 @@ class HomePage extends StatelessWidget {
           appBar: AppBar(
             title: const Text('Home Page'),
             actions: [
-              ElevatedButton(onPressed: _signOutAnonymously, child: const Text('Logout',style: TextStyle(fontSize: 18.0,color: Colors.white),))
+              ElevatedButton(onPressed: ()=>_signOut(), child: const Text('Logout',style: TextStyle(fontSize: 18.0,color: Colors.white),))
             ],
           ),
         );
   }
-
-
 }
--- lib/common_widgets/show_alert_dialog.dart
+++ lib/common_widgets/show_alert_dialog.dart
@@ -1,29 +1,41 @@
 import 'dart:io';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-void showAlertDialog({
+Future<bool?>? showAlertDialog({
   required BuildContext context,
   required String title,
+  String? cancelText,
   required String content,
   required String defaultActionText,
-}) {
+})  async {
   if (Platform.isAndroid) {
-    showDialog(
+     showDialog(
+       barrierDismissible: false,
         context: context,
         builder: (context) {
           return AlertDialog(
+
             title: Text(title),
             content: Text(content),
             actions: [
-              FlatButton(
-                  onPressed: () => Navigator.of(context).pop(true),
+              if(cancelText!=null)
+              ElevatedButton(
+                  onPressed: () {
+                    print('cancel pressed');
+                    return Navigator.of(context).pop(false);
+                  },
+                  child:  Text(cancelText!)),
+              ElevatedButton(
+                  onPressed: (){
+                    print('ok pressed');
+                    return Navigator.of(context).pop(true);
+
+                  },
                   child: Text(defaultActionText))
             ],
           );
         });
-  } else if (Platform.isIOS) {
+  }else {
     showCupertinoDialog(
         context: context,
         builder: (context) {
@@ -31,6 +43,10 @@ void showAlertDialog({
             title: Text(title),
             content: Text(content),
             actions: [
+              if(cancelText!=null)
+                CupertinoDialogAction(
+                    onPressed: () => Navigator.of(context).pop(false),
+                    child: Text(cancelText)),
               CupertinoDialogAction(
                   onPressed: () => Navigator.of(context).pop(true),
                   child: Text(defaultActionText))
